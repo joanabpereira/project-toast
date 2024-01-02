325918ea733d67d8b2bb8b76ef158e27a3a78d56
Joana Borges <pereira.b.joana@gmail.com>
2024-01-02 21:04:32 +0000

    feat: add editable toast preview

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -18,16 +18,22 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
+function Icon({icon: Icon}){
+  return(
+      <Icon size={24} />
+  );
+}
+
+function Toast({variant, onClose, children}) {
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+    <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <Icon icon={ICONS_BY_VARIANT[variant]}/>
       </div>
       <p className={styles.content}>
-        16 photos have been uploaded
+        {children}
       </p>
-      <button className={styles.closeButton}>
+      <button className={styles.closeButton} onClick={()=>onClose(false)}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
